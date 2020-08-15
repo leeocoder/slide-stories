875c2285eb43363f16673ab5825467c421c22539
Leonardo Albuquerque <leeodesign@hotmail.com>
2020-08-15 19:18:49 -0300

    JS Get all images of slide and put class active and get of current slide active

---
+++ slide-stories.js
@@ -0,0 +1,16 @@
+class SlideStories {
+  constructor(id) {
+    this.slide = document.querySelector(`[data-slide='${id}']`);
+    this.active = 0;
+    console.log(this.slide);
+    this.activeSlide(1);
+  }
+
+  activeSlide(index) {
+    this.active = index;
+    this.items = this.slide.querySelectorAll(".slide-items > *");
+    this.items[index].classList.add("active");
+  }
+}
+
+new SlideStories("slide");
