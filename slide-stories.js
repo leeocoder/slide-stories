1a0fec0acd16f4c727c6ab8be3339c0319035973
Leonardo Albuquerque <leeodesign@hotmail.com>
2020-08-15 19:49:44 -0300

    *JS Slide manual all done, event click on buttons

--- slide-stories.js
+++ slide-stories.js
@@ -3,13 +3,43 @@ class SlideStories {
     this.slide = document.querySelector(`[data-slide='${id}']`);
     this.active = 0;
     console.log(this.slide);
-    this.activeSlide(0);
+    this.init();
   }
 
   activeSlide(index) {
     this.active = index;
-    this.items = this.slide.querySelectorAll(".slide-items > *");
+    this.items.forEach((item) => item.classList.remove("active"));
     this.items[index].classList.add("active");
+  }
+
+  addNavigation() {
+    const nextBtn = this.slide.querySelector(".slide-next");
+    const prevBtn = this.slide.querySelector(".slide-prev");
+    nextBtn.addEventListener("click", this.next);
+    prevBtn.addEventListener("click", this.prev);
+  }
+
+  prev() {
+    if (this.active > 0) {
+      this.activeSlide(this.active - 1);
+    } else {
+      this.activeSlide(this.items.length - 1);
+    }
+  }
+  next() {
+    if (this.active < this.items.length - 1) {
+      this.activeSlide(this.active + 1);
+    } else {
+      this.activeSlide(0);
+    }
+  }
+
+  init() {
+    this.next = this.next.bind(this);
+    this.prev = this.prev.bind(this);
+    this.items = this.slide.querySelectorAll(".slide-items > *");
+    this.activeSlide(0);
+    this.addNavigation();
   }
 }
 
